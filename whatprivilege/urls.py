a07b9ffed98d3bcecb364d15671db809adadfc94
evykassirer <evy.kassirer@gmail.com>
2015-02-14 17:18:38 -0500

    base question page, but css doesn't work

--- whatprivilege/urls.py
+++ whatprivilege/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, include, url
 from whatprivilege import views
+from django.conf.urls.static import static
+
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
+    url(r'^question/$', views.question, name='question'),
     url(r'^instructions/$', views.instructions, name='instructions')    
 # url(r'^answer/(number)/$', 'views.answer', name='answer')
 )
--- whatprivilege/views.py
+++ whatprivilege/views.py
@@ -9,9 +9,9 @@ def instructions(request):
     #logics....
     return render_to_response('instructions.html',{})
 
-def questions(request):
+def question(request):
     #logics...
-    return render_to_response('questions.html', {}) 
+    return render_to_response('question.html', {"question":"test"}) 
 
 def learned(request):
     #logics...
