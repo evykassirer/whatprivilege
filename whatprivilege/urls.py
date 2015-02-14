47a7cc48ee7916106dc0ea40bb27b40cf91dd021
Evy <evy.kassirer@gmail.com>
2015-02-14 15:32:27 -0500

    Created proper template directory.

--- whatprivilege/urls.py
+++ whatprivilege/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from whatprivilege import views
 urlpatterns = patterns('',
-    url(r'^$', 'views.home', name='home'),
-    url(r'^instructions/$', name='instructions')    
+    url(r'^$', views.home, name='home'),
+    url(r'^instructions/$', views.instructions, name='instructions')    
 # url(r'^answer/(number)/$', 'views.answer', name='answer')
 )
--- whatprivilege/views.py
+++ whatprivilege/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render_to_response
 
 def home(request):
     #logics .... 
-    return render_to_response('templates/home.html', {}) 
+    return render_to_response('home.html', {}) 
 
 def instructions(request): 
     #logics....
-    return render_to_response('templates/instructions.html',{})
+    return render_to_response('instructions.html',{})
