8b81c5622842a1e307133b7a5564ce9d014ddf97
Anna <aemhlori@uwaterloo.ca>
2015-02-14 14:23:44 -0500

    Created home and instructions views

--- whatprivilege/urls.py
+++ whatprivilege/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from whatprivilege import views
 urlpatterns = patterns('',
     url(r'^$', 'views.home', name='home'),
-    # url(r'^answer/(number)/$', 'views.answer', name='answer')
+    url(r'^instructions/$', name='instructions')    
+# url(r'^answer/(number)/$', 'views.answer', name='answer')
 )
--- whatprivilege/views.py
+++ whatprivilege/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+#from whatprivilege.models import Question
 
-# Create your views here.
+def home(request):
+    #logics .... 
+    return render_to_response('templates/home.html', {}) 
+
+def instructions(request): 
+    #logics....
+    return render_to_response('templates/instructions.html',{})
