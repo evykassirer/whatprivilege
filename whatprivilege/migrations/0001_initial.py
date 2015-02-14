5414795cc3eceb8539d28eb07d3e2014244c0ec6
Anna <aemhlori@uwaterloo.ca>
2015-02-14 17:14:04 -0500

    Adding changes. Merging.

---
+++ whatprivilege/migrations/0001_initial.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Question',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('questionText', models.TextField()),
+                ('helpText', models.TextField()),
+                ('helpLink', models.URLField()),
+                ('numberYes', models.IntegerField()),
+                ('numberNo', models.IntegerField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
