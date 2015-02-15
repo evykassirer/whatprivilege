1dac2a866fac6a45832985bfc9d2282dfa6ac41a
evykassirer <evy.kassirer@gmail.com>
2015-02-15 08:27:23 -0500

    updated admins files

--- whatprivilege/admin.py
+++ whatprivilege/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from whatprivilege.models import Question
+from whatprivilege.models import WorkshopQuestion
+from whatprivilege.models import Workshop
 # Register your models here. 
 
 admin.site.register(Question)
+admin.site.register(WorkshopQuestion)
+admin.site.register(Workshop)
