b0b11f0a2c5acf61983b917624bd946b3b46ebac
Joey Rideout <joeyrrideout@gmail.com>
2015-05-13 22:56:24 -0400

    Adding a missing send_mail import in views.py

--- whatprivilege/views.py
+++ whatprivilege/views.py
@@ -1,4 +1,5 @@
 """All request-handling logic for whatprivilege app."""
+from django.core.mail import send_mail
 from django.http import Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
